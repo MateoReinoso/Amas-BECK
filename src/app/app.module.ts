3a1055d0f86deed80dd78ca07d706af47ab6c352
Mateo <mateo_riva@hotmail.com>
2020-12-04 23:42:39 -0500

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { RutasPrincipales } from './constantes/rutas-principales';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.coomponent';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ModalComponent } from './rutas-principales/componentes/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ModalComponent,
     ...RutasPrincipales
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    
+    MatDialogModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/rutas-principales/ruta-inicio/ruta-inicio.component.ts
+++ src/app/rutas-principales/ruta-inicio/ruta-inicio.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ModalComponent } from '../componentes/modal/modal.component'
+
 
 @Component({
     selector: 'ruta-inicio',
     templateUrl: './ruta-inicio.component.html',
-    styleUrls:['./ruta-inicio.component.scss']    
+    styleUrls: ['./ruta-inicio.component.scss']
 })
-export class RutaInicioComponent{
-    
+export class RutaInicioComponent {
+    constructor(
+        private readonly _dialog: MatDialog
+    ) { }
+
+    abrirModal() {
+        const opcionesModal: MatDialogConfig = {
+            disableClose: true,
+            height: '450px',
+            width: '900px',
+        }
+
+        const modal = this._dialog.open(ModalComponent, opcionesModal)
+
+        modal.afterClosed().subscribe((respuestaModal) => {
+            console.log('respuesta modal: ', respuestaModal);
+      
+            if (respuestaModal) {
+              console.log('no es undefined');
+            } else {
+              console.log(' es undefined');
+            }
+          });
+    }
 }
