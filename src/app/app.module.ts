e0b92301ff0e6c7dc99dba9b5dc049405a3afc1a
Mateo <mateo_riva@hotmail.com>
2020-12-04 19:32:24 -0500

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatDialogModule} from "@angular/material/dialog";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RutasPrincipales } from './constantes/rutas-principales';
@@ -11,7 +10,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    MatDialogModule,
     HeaderComponent,
     FooterComponent,
     ...RutasPrincipales
