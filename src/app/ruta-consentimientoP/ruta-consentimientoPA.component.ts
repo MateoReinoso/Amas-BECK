438f6375519aaddb2e7c705602a39f186a094889
DANIEL MATEO REINOSO RIVADENEIRA <dmreinoso2@espe.edu.ec>
2021-02-22 14:53:20 -0500

    consentimiento-fomr

--- src/app/ruta-consentimientoP/ruta-consentimientoPA.component.ts
+++ src/app/ruta-consentimientoP/ruta-consentimientoPA.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "ruta-consentimientoP",
@@ -7,5 +8,9 @@ import { Component } from "@angular/core";
 })
 
 export class RutaConsentimientoPAComponent{
+    constructor(private router:Router){}
 
+    irEncuesta(){
+        this.router.navigate(['/','encuestapa']);
+    }
 }
